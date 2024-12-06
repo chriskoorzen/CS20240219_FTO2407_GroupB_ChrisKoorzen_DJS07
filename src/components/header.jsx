2e8e683bf7353a81bdd592df46bc67e465ffc8b6
Chris Koorzen <fckoorzen@gmail.com>
2024-12-06 12:40:17 +0200

    Remove project details from header.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,11 @@ import trollface from "../../static/troll-face.png"
 
 export default function Header() {
     return (
-        <header className="h-16 p-4 flex flex-row justify-around text-white app-gradient">
+        <header className="h-16 py-4 px-20 text-white app-gradient">
             <div className="h-full flex flex-row">
                 <img src={trollface} />
                 <h2 className="ml-3 font-bold text-xl">Meme Generator</h2>
             </div>
-            <h4 className="">CodeSpace Academy - Chris Koorzen - DJS 07</h4>
         </header>
     )
 }
